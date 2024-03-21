696c1171149d3a7bb7e92364976d85f7e261f79e
Yurii <iyureg@gmail.com>
2024-03-21 10:59:18 +0300

    modified: build.gradle.kts

--- Junior/Junior/build.gradle.kts
+++ Junior/Junior/build.gradle.kts
@@ -19,6 +19,17 @@ dependencies {
     // https://mvnrepository.com/artifact/com.fasterxml.jackson.core/jackson-databind
     implementation("com.fasterxml.jackson.core:jackson-databind:2.16.0")
 
+    // https://mvnrepository.com/artifact/com.mysql/mysql-connector-j
+    implementation("com.mysql:mysql-connector-j:8.3.0")
+
+    // https://mvnrepository.com/artifact/org.hibernate.orm/hibernate-core
+    implementation("org.hibernate.orm:hibernate-core:6.4.4.Final")
+
+
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
 }
 
 tasks.test {
